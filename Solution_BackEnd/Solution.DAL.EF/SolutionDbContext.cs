810e76c8cfa6ff296f732c5f38d1be3762d12173
LAPTOP-NTJ8C71K\Fabian <rojascamachof@gmail.com>
2021-03-10 21:24:24 -0600

    Primer Commit
    Incluye la estructura de carpetas necesarias para la parte de la creacion de las APIs, ademas incluye la clase ICRUD, Repository y SolutionDbContext(Vacio) y la inteface IRepository, tambien incluye la carpeta de FrontEnd(Vacia)

---
+++ Solution_BackEnd/Solution.DAL.EF/SolutionDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Solution.DO.Objects;
+
+namespace Solution.DAL.EF
+{
+    public partial class SolutionDbContext : DbContext
+    {
+        public SolutionDbContext(DbContextOptions<SolutionDbContext> options) : base(options)
+        {
+
+        }
+   
+        //DBSet van aqui
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            //model builder va aqui
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+    }
+}
---
+++ Solution_BackEnd/Solution.DAL.Repository/Repository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Solution.DO.Objects;
+
+namespace Solution.DAL.EF
+{
+    public partial class SolutionDbContext : DbContext
+    {
+        public SolutionDbContext(DbContextOptions<SolutionDbContext> options) : base(options)
+        {
+
+        }
+ 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+     
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+    }
+}
