931b854d4392840e7fc70a9b5e386a4537810cc9
LAPTOP-NTJ8C71K\Fabian <rojascamachof@gmail.com>
2021-03-15 18:17:08 -0600

    Archivos que faltaban
    Archivos que faltaban

---
+++ Solution_BackEnd/Solution.API/DataModels/CorreoTienda.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Solution.API.DataModels
+{
+    public class CorreoTienda
+    {
+        public decimal IdCorreo { get; set; }
+        public string Correo { get; set; }
+        public string Descripcion { get; set; }
+        public decimal IdTienda { get; set; }
+
+        //public virtual Tiendas IdTiendaNavigation { get; set; }
+    }
+}
--- Solution_BackEnd/Solution.API/Mapping/MappingProfile.cs
+++ Solution_BackEnd/Solution.API/Mapping/MappingProfile.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using data = Solution.DO.Objects;
 
 namespace Solution.API.Mapping
 {
-    public class MappingProfile
+    public class MappingProfile : Profile
     {
+        public MappingProfile()
+        {
+
+          CreateMap<data.CategoriaProductos, DataModels.Course>().ReverseMap();
+
+        
+        }
     }
 }
