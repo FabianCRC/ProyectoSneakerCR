46152bf1073c88f30cf82de5d151050264951711
LAPTOP-NTJ8C71K\Fabian <rojascamachof@gmail.com>
2021-03-15 18:59:12 -0600

    DataModels, MappingProfile,String de conexionbd
    DataModels, MappingProfile,String de conexionbd

---
+++ Solution_BackEnd/Solution.API/DataModels/TelefonoTienda.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Solution.API.DataModels
+{
+    public class TelefonoTienda
+    {
+        public decimal IdTelefono { get; set; }
+        public string Descripcion { get; set; }
+        public string Numero { get; set; }
+        public decimal IdTienda { get; set; }
+
+        //public virtual Tiendas IdTiendaNavigation { get; set; }
+    }
+}
--- Solution_BackEnd/Solution.API/Mapping/MappingProfile.cs
+++ Solution_BackEnd/Solution.API/Mapping/MappingProfile.cs
@@ -1,11 +1,24 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using data = Solution.DO.Objects;
 
 namespace Solution.API.Mapping
 {
-    public class MappingProfile
+    public class MappingProfile : Profile
     {
+        public MappingProfile()
+        {
+
+            CreateMap<data.CategoriaProductos, DataModels.CategoriaProductos>().ReverseMap();
+            CreateMap<data.CorreoTienda, DataModels.CorreoTienda>().ReverseMap();
+            CreateMap<data.MarcaProductos, DataModels.MarcaProductos>().ReverseMap();
+            CreateMap<data.Productos, DataModels.Productos>().ReverseMap();
+            CreateMap<data.Roles, DataModels.Roles>().ReverseMap();
+            CreateMap<data.TelefonoTienda, DataModels.TelefonoTienda>().ReverseMap();
+
+
+        }
     }
-}
